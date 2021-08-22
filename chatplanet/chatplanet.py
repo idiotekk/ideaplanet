72f87eda21acc5a4bdb2bab5e57768fa143305a6
zche <cheziliang@gmail.com>
2021-08-22 00:25:48 -0500

    add Bach

---
+++ chatplanet/chatplanet.py
@@ -0,0 +1,19 @@
+import socket
+from threading import Thread
+
+# server's IP address
+SERVER_HOST = "0.0.0.0"
+SERVER_PORT = 5002 # port we want to use
+separator_token = "<SEP>" # we will use this to separate the client name & message
+
+# initialize list/set of all connected client's sockets
+client_sockets = set()
+# create a TCP socket
+s = socket.socket()
+# make the port as reusable port
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# bind the socket to the address we specified
+s.bind((SERVER_HOST, SERVER_PORT))
+# listen for upcoming connections
+s.listen(5)
+print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
