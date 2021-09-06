d96fa0cbe79fb1d2ced4ed13214116d5752ecb91
Z.Che <cheziliang@gmail.com>
2021-09-06 01:10:05 -0500

    Update donwload_podcast.py
    comment

--- webscraper/donwload_podcast.py
+++ webscraper/donwload_podcast.py
@@ -4,14 +4,14 @@ from selenium import webdriver
 # the 2 lines below are for printing logging messages 
 import logging as log
 log.basicConfig(level=log.INFO)
-# the 3 lines below makes browser invisible
+# the 3 lines below make browser invisible
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 
 def download_audio(url=None, out_file=None):
-    """ Downloads an auodio file from a url to a file. """
+    """ This function downloads an audio file from a url to a file. """
     file_ = requests.get(url)
     log.info(f"downloading {url} to {out_file}")
     with open(out_file, "wb") as f:
@@ -35,13 +35,13 @@ Example:
 
     with webdriver.Chrome(options=chrome_options) as driver: # open a browser 
 
-        # open the page and find the link to embedded player
+        # step1: open the page and find the link to embedded player
         log.info(f"opening webpage: {args.url}")
         driver.get(args.url)
         elem = driver.find_element_by_tag_name("iframe")
         player_url = elem.get_attribute("src")
 
-        # open the embedded player and find the link to the mp3
+        # step2: open the embedded player and find the link to the mp3
         log.info(f"redirecting to: {player_url}")
         driver.get(player_url)
         for elem in driver.find_elements_by_class_name("MenuBar__btn"):
@@ -49,6 +49,6 @@ Example:
             if href and "mp3" in href:
                 audio_url = href # this is the link to the mp3
 
-    # save mp3 to your pc
+    # step3: save mp3 to your pc
     file_name = args.url.strip("/").split("/")[-1]
     download_audio(url=audio_url, out_file=(f"{args.out_dir}/{file_name}.mp3"))
