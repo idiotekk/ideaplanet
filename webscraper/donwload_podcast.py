d3fcee7eada6c0f80349162017814ff6584f316a
zche <cheziliang@gmail.com>
2021-09-06 00:29:58 -0500

    add

--- webscraper/donwload_podcast.py
+++ webscraper/donwload_podcast.py
@@ -1,16 +1,16 @@
-import requests, os
+import os
+import requests
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+# the 2 lines below are for printing logging messages 
 import logging as log
-from argparse import ArgumentParser, RawTextHelpFormatter
 log.basicConfig(level=log.INFO)
 # the 2 lines below makes browser invisible
+from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
 def download_audio(url=None, out_file=None):
-    """ Downloads an auodio file from a url to a file.
-    """
+    """ Downloads an auodio file from a url to a file. """
     file_ = requests.get(url)
     log.info(f"downloading {url} to {out_file}")
     with open(out_file, "wb") as f:
@@ -18,6 +18,7 @@ def download_audio(url=None, out_file=None):
 
 if __name__ == "__main__":
 
+    from argparse import ArgumentParser, RawTextHelpFormatter
     parser = ArgumentParser(description="""This script downloads audio files from https://newbooksnetwork.com/
 Example:
     donwload_podcast.py https://newbooksnetwork.com/emilie-hafner-burton-improving-human-rights-open-agenda-2021 """, formatter_class=RawTextHelpFormatter)
@@ -27,11 +28,12 @@ Example:
 
     with webdriver.Chrome(options=chrome_options) as driver:
 
+        log.info(f"opening webpage: {args.url}")
         driver.get(args.url)
         elem = driver.find_element_by_tag_name("iframe")
         player_url = elem.get_attribute("src")
 
-        log.info(f"redirecting to {player_url}")
+        log.info(f"redirecting to: {player_url}")
         driver.get(player_url)
         for elem in driver.find_elements_by_class_name("MenuBar__btn"):
             href = elem.get_attribute("href")
@@ -40,4 +42,3 @@ Example:
 
     file_name = args.url.strip("/").split("/")[-1]
     download_audio(url=audio_url, out_file=(f"{args.out_dir}/{file_name}.mp3"))
-
