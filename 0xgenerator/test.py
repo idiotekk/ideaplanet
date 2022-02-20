43ba73b2eef4385e12b49dd40d95d4bc6bfd5813
zche <cheziliang@gmail.com>
2022-02-20 12:01:19 -0600

    simplify

--- 0xgenerator/test.py
+++ 0xgenerator/test.py
@@ -1,29 +1,11 @@
-from codecs import BOM_UTF16_BE
-import glob
 import numpy as np
 from pathlib import Path
 from PIL import Image
 import logging as log
 from tqdm import tqdm
+from PIL import ImageFilter
+from argparse import ArgumentParser
 log.basicConfig(level=log.DEBUG)
-
-#    pics = glob.glob(f"{input_dir}/*.png")
-#    frames = [Image.open(_) for _ in pics]
-#    frame_one = frames[0]
-#    output_file = f"{output_dir}/test0.gif" 
-#    frame_one.save(output_file,
-#                   format="GIF", 
-#                   append_images=frames[1:],
-#                   save_all=True, 
-#                   duration=500, 
-#                   loop=1)
-#    log.info(output_file)
-
-
-def open_frames(files, input_dir, suffix=".jpg"):
-    return {
-        _ : Image.open(f"{input_dir}/{_}{suffix}") for _ in files
-    }
 
 
 def compile(frames, *, output_file, duration):
@@ -38,49 +20,24 @@ def compile(frames, *, output_file, duration):
     log.info(output_file)
 
 
-def same_color(x, y, tol=2):
-    """ True if x and y are almost the same color.
-    """
-    return all(abs(i-j)<= tol for i, j in zip(x, y))
-
-
-def yellow_to_black(im):
-    newimdata = []
-    yellow = (255, 215, 0)
-    blackcolor = (0,0,0)
-    for color in im.getdata():
-        if same_color(color, yellow, tol=30):
-            newimdata.append(blackcolor)
-        else:
-            newimdata.append(color)
-    newim = Image.new(im.mode, im.size)
-    newim.putdata(newimdata)
-    return newim
-
-
-
-def read_gif(path, *, samples, resize, crop=1.0):
+def read_gif(path, *, samples, size, rescale=1.0):
     
-    log.info(f"about to read {path}")
     gif_image = Image.open(path)
-    log.info(f"finished reading {path}")
-
     n_frames = gif_image.n_frames
     log.info(f"total number of frames = {n_frames}")
-    frames = []
-    log.info(f"extracting frames from gif")
 
     #for i in tqdm(range(n_frames)):
     x_0, y_0 = gif_image.size[0] // 2, gif_image.size[1] // 2
-    tmp = int((min(x_0, y_0) - 1) * crop)
+    tmp = int((min(x_0, y_0) - 1) * rescale)
     crop_limits = (x_0 - tmp, y_0 - tmp, x_0 + tmp, y_0 + tmp)
     log.info(f"cropped at: {crop_limits}")
     
+    frames = []
     for i in range(n_frames):
         gif_image.seek(i)
         frame = gif_image.convert("RGB")
         frame = frame.crop(crop_limits)
-        frame = frame.resize(resize, Image.ANTIALIAS)
+        frame = frame.resize(size, Image.ANTIALIAS)
         frames.append(frame)
     
     if n_frames < samples:
@@ -95,41 +52,40 @@ def read_gif(path, *, samples, resize, crop=1.0):
 def replace_yellow_by_non_white(data1, data2):
     """ Replace the yellow in im1 by im2 if im2 is not white.
     """
-    newimdata = []
-    yellow = (255, 215, 0)
-    white = (255, 255, 255)
-    is_yellow = (np.max(np.abs(data1 - np.array(yellow)), axis=2) <  10)
-    not_white = (np.max(np.abs(data2 - np.array(white )), axis=2) >= 30)
-    replace = np.logical_and(is_yellow, not_white)
-    replace = np.stack([replace]*3, axis=2)
-    new_data = np.where(replace, data2, data1)
+    #yellow  = (255, 215, 0)
+    yellow  = data1[5, 5, :]
+    white   = (255, 255, 255)
+    is_yellow   = (np.max(np.abs(data1 - np.array(yellow)), axis=2) <  10)
+    not_white   = (np.max(np.abs(data2 - np.array(white )), axis=2) >= 30)
+    replace     = np.stack([np.logical_and(is_yellow, not_white)] * 3, axis=2)
+    new_data    = np.where(replace, data2, data1)
     return new_data
 
+def main(fg_file, input_dir, output_file):
 
-def main(input_dir, output_file):
-
-    n_frames = 200
-    #fg_frames = open_frames(["p1", "p2", "p3"], input_dir, suffix=".jpg")
-    fg_frame = Image.open(f"{input_dir}/p1.jpg")
-    w, h = fg_frame.size
-    bg_frames = read_gif(f"{input_dir}/sakura.gif", samples=n_frames, resize=(w, h))
+    n_frames    = 60
+    size        = (500, 500)
+    fg_frame    = Image.open(fg_file)
+    fg_frame    = fg_frame.resize(size).convert("RGB")
+    bg_frames   = read_gif(f"{input_dir}/sakura.gif", samples=n_frames, size=fg_frame.size, rescale=0.8)
 
     # translate sequence of label to sequence of frames
     frames = []
     im_data1 = np.array(fg_frame)
-    for i, bg_frame in tqdm(enumerate(bg_frames)):
+    for bg_frame in tqdm(bg_frames):
         im_data2 = np.array(bg_frame)
         new_data = replace_yellow_by_non_white(im_data1, im_data2)
         frame = Image.fromarray(new_data.astype('uint8'), "RGB")
+        frame = frame.filter(ImageFilter.SMOOTH)
         frames.append(frame)
 
     # set duration of each frame according total time and number of frames
-    total_time = 10000
+    total_time = 5000
     duration = total_time / len(frames)
-    log.info(duration)
-
-    # compile
     compile(frames, output_file=output_file, duration=duration)
+
+parser = ArgumentParser()
+parser.add_argument("file_name", type=str)
     
 if __name__ == "__main__":
 
@@ -137,4 +93,9 @@ if __name__ == "__main__":
     input_dir = f"{root_dir}/inputs"
     output_dir = f"{root_dir}/output"
     Path(output_dir).mkdir(parents=True, exist_ok=True)
-    main(input_dir, f"{output_dir}/sakura.gif")
+    #fg_file = f"{input_dir}/p1.jpg"
+
+    #file_name = "p4" # file name without suffix
+    file_name = parser.parse_args().file_name
+    fg_file = f"{input_dir}/{file_name}.png"
+    main(fg_file, input_dir, f"{output_dir}/{file_name}_.gif")
