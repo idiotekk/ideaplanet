7715b9ea85124766e8c639ea9c674482b7b93ddb
ziliac <cheziliang@gmai.com>
2022-03-12 11:51:45 -0600

    nightmare

--- 0xgenerator/python/scripts/bg_switch.py
+++ 0xgenerator/python/scripts/bg_switch.py
@@ -19,14 +19,14 @@ from math import pi
 log.basicConfig(level=log.DEBUG)
 np.set_printoptions(edgeitems=8, linewidth=100000)
 
-input_dir = Path("/Users/zche//data/0xgenerator/bg_switch/inputs/")
-output_dir = Path("/Users/zche//data/0xgenerator/bg_switch/outputs/") 
+input_dir = Path("/Users/zche/cloud/data/0xgenerator/bg_switch/inputs/")
+output_dir = Path("/Users/zche/cloud/data/0xgenerator/bg_switch/outputs/") 
 
 def bg_switch(file_name):
 
     #input_arr = io.read_frame(input_dir / file_name, to_np=True, size=(600, 600))
     #input_arr = io.read_frame(input_dir / file_name, to_np=True, size=(1121, 1121))
-    input_arr = io.read_frame(input_dir / file_name, to_np=True, size=(1121, 1121))
+    input_arr = io.read_frame(input_dir / file_name, to_np=True)
     bg_arrs = [io.read_frame(_) for _ in glob(str(input_dir / "azuki/*"))]
     np.random.shuffle(bg_arrs)
     bg_arrs.insert(4, input_arr)
@@ -34,8 +34,8 @@ def bg_switch(file_name):
     
     margin0 = 0.01
     margin1 = 0.01
-    #grid_w, grid_h = 3, 3
-    grid_w, grid_h = 1, 1
+    grid_w, grid_h = 3, 3
+    #grid_w, grid_h = 1, 1
     output_size = (
         int(grid_h * input_h * (1 + 2*margin1) + input_h * margin0 * 2), 
         int(grid_w * input_w * (1 + 2*margin1) + input_w * margin0 * 2))
@@ -48,26 +48,28 @@ def bg_switch(file_name):
     is_bg_color = frm.is_same_color(input_arr, color=input_bg_color, tol=3)
     is_black = (
         np.logical_or.reduce((
-            frm.is_same_color(input_arr, color=frm.BLACK, tol=50),
-            frm.is_same_color(input_arr, color=frm.WHITE, tol=100),
+            frm.is_same_color(input_arr, color=frm.BLACK, tol=30),
+            frm.is_same_color(input_arr, color=frm.WHITE, tol=150),
             input_arr[:,:,2] > input_arr[:,:,1], # blue-ish
             )
         )
     )
 
     # flag2: has yellow neighbor
-    all_neighbors_are_yellow = np.zeros(is_bg_color.shape)
-    rr = 1
+    yellow_neighbor_count = np.zeros(is_bg_color.shape)
+    rr = 2
     count = 0
     for i in range(-rr, rr+1):
         for j in range(-rr, rr+1):
-            all_neighbors_are_yellow += np.roll(is_bg_color, (i, j), (0, 1))
+            yellow_neighbor_count += np.roll(is_bg_color, (i, j), (0, 1))
             count += 1
-    all_neighbors_are_yellow  = (all_neighbors_are_yellow >= count)
+    all_neighbors_are_yellow  = (yellow_neighbor_count >= count)
+    #is_isolated_yellow  = (yellow_neighbor_count <= count*0.2)
     visited = all_neighbors_are_yellow
     print(np.sum(visited) - np.sum(is_bg_color))
 
     flag = is_bg_color.copy()
+    is_boundary = np.logical_and(False, flag) # all false
 
     def spread(i, j, depth, visited):
         if visited[i, j] == True:
@@ -80,9 +82,13 @@ def bg_switch(file_name):
                 j_new = j + dj
                 if i_new < 0 or i_new >= input_h or j_new < 0 or j_new >= input_w:
                     continue
-                flag[i_new, j_new] = True
-                if is_black[i_new, j_new] == False:
+                if is_black[i_new, j_new] == False or np.logical_and(
+                    input_arr[i_new,j_new,2] < input_arr[i_new,j_new,1] < input_arr[i_new,j_new,0],
+                    j > input_w * 0.6
+                ): # yellow-ish
                     flag[i_new, j_new] = True
+                    if is_bg_color[i_new, j_new] == False:
+                        is_boundary[i_new, j_new] = True
                     spread(i_new, j_new, depth - 1, visited)
         visited[i, j] = True
 
@@ -100,9 +106,11 @@ def bg_switch(file_name):
         bg_arr = bg_arrs[idx]
         bg_color = frm.get_bg_color(bg_arr)
         new_arr = frm.where(flag, bg_color, input_arr)
+        blurred_arr = np.array(io.np_to_im(new_arr).filter(ImageFilter.GaussianBlur))
+        new_arr = frm.where(is_boundary, blurred_arr, new_arr)
         #new_arr = frm.where(flag, bg_color, frm.WHITE) # test
         #new_arr = frm.where(is_black, frm.BLACK, new_arr) # test
-        test_row = int(input_h * 0.35)
+        #test_row = int(input_h * 0.35)
         #pd.DataFrame(input_arr[input_h // 2,:,:], columns=["r", "g", "b"]).to_csv(str(output_dir/"inputslice.csv"))
         #pd.DataFrame(new_arr[test_row,:,:], columns=["r", "g", "b"]).to_csv(str(output_dir/"slice.csv"))
         #new_arr[test_row,:,:] = canvas_color
@@ -114,7 +122,8 @@ def bg_switch(file_name):
             int(j * (input_w*(1+2*margin1)) + input_w * margin1 + input_w * margin0)
         ))
 
-    output_im = output_im.filter(ImageFilter.SMOOTH_MORE)
+    #output_im = output_im.filter(ImageFilter.SMOOTH_MORE)
+    output_im = output_im.filter(ImageFilter.SMOOTH)
     output_file = Path(output_dir) / f"{file_name.split('.')[0]}_all.png"
     output_file.parent.mkdir(parents=True, exist_ok=True)
     io.save_im(output_im, output_file)
